8805b7f5c6e189afe4beaf1a0b2b3e3f008e0761
RezaCharrakh <116076795+RezaCharrakh@users.noreply.github.com>
2024-11-01 20:48:12 +0330

    update sgd

---
+++ phase1/Linear_regression/main.py
@@ -0,0 +1,23 @@
+import numpy as np
+from sgd.sgd import SGD
+from sgd.utils import load_data, preprocess_data, normalize_features
+from sgd.metrics import mean_squared_error, r2_score
+
+def main():
+    train_df = load_data('D:/Git Projects/search-and-machine-learning-fakesmart/phase1/Linear_regression/data/train.csv')
+    test_df = load_data('D:/Git Projects/search-and-machine-learning-fakesmart/phase1/Linear_regression/data/test.csv')
+    X_train, y_train = preprocess_data(train_df)
+    X_test, y_test = preprocess_data(test_df)
+    X_train = normalize_features(X_train)
+    X_test = normalize_features(X_test)
+
+    model = SGD(lr=0.001, epochs=100, batch_size=512, tol=1e-3, momentum=0.9, l2_lambda=0.001, decay_rate=0.95)
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    test_loss = mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+    print(f"Test Loss: {test_loss}, R² Score: {r2}")
+
+if __name__ == '__main__':
+    main()
